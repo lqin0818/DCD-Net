91a531bef413d597a1fe2fcb3470dba8b018b2ab
Leo <90945300+lqin0818@users.noreply.github.com>
2025-10-03 18:40:28 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ model = YOLO("../DCD/ultralytics/cfg/models/DCD.yaml")
 
 if __name__ == '__main__':
 
-    model.train(data = "pcb_component.yaml", batch = 2, epochs = 100, imgsz = 640, project='DCD')
+    model.train(data = "pcb_component.yaml", batch = 16, epochs = 100, imgsz = 640, project='DCD')
 
     #results = model.val()
     #predicts = model.predict("bus.jpg", save=True)
